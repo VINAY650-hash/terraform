b65b7ea8790c3285695833230f890306098b4a03
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2024-01-26 19:46:36 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -9,10 +9,4 @@ terraform {
   }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "TF-Instance"
-  }
-}
+
