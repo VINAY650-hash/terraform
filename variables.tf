eb9c6d76ab757350c4ba3b59c2ed7cee1d5247cb
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2024-01-26 19:45:21 +0530

    Update Jenkinsfile

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ pipeline {
                  script{
                         dir("terraform")
                         {
-                            git "https://github.com/yeshwanthlm/Terraform-Jenkins.git"
+                           git https://github.com/VINAY650-hash/terraform
                         }
                     }
                 }
