ed0a3467935a411a7c17ec8cebe887e950bf00d2
VINAY650-hash <79817305+VINAY650-hash@users.noreply.github.com>
2024-01-27 09:34:33 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-0f5ee92e2d63afc18"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
